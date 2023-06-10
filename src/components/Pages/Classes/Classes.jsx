93489bcd8e1b0071b68495fbad663c28421f600d
Abu-Jayed <aj.webdevlopment@gmail.com>
2023-06-10 19:08:50 +0600

    selected class added to DataBase

--- src/components/Pages/Classes/Classes.jsx
+++ src/components/Pages/Classes/Classes.jsx
@@ -1,8 +1,10 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { BsStarFill, BsStarHalf } from "react-icons/bs";
 import Swal from "sweetalert2";
+import { AuthContext } from "../../Provider/AuthProvider";
 
 const Classes = () => {
+    const {user} = useContext(AuthContext)
     const [classes, setClasses] = useState([])
 
     useEffect(() => {
@@ -10,7 +12,20 @@ const Classes = () => {
             .then(res => res.json())
             .then(data => setClasses(data))
     }, [])
-    const handleOrderConfirm = () => {
+    const handleSelectClass = (classes) => {
+        console.log(classes);
+        const {availableSeats,classImage,className,enrolled,instructorName,instructorEmail,price,rating,_id} = classes
+        // console.log(availableSeats);
+        if(user){
+            const selectedClass = {classId: _id, instructorName, classImage, price, email: user.email,rating,enrolled,className,instructorEmail,availableSeats}
+            fetch('http://localhost:5000/selectedClass', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(selectedClass)
+            })
+        }
         Swal.fire({
             position: "top-center",
             icon: "success",
@@ -31,7 +46,7 @@ const Classes = () => {
                         return <>
 
                             <div key={fightClass.classImage}>
-                                <div className={`shadow-md hover:shadow-md hover:shadow-black shadow-gray-600 ${fightClass.availableSeats === 0 ? 'shadow-red-700  hover:shadow-red-500 hover:shadow-lg':''}`}>
+                                <div className={`shadow-md hover:shadow-md hover:shadow-black shadow-gray-600 ${fightClass.availableSeats === 0 ? 'shadow-red-700  hover:shadow-red-500 hover:shadow-lg' : ''}`}>
                                     <img className="mx-auto w-[340px] h-[226px]" src={fightClass.classImage} alt="" />
                                     <a href="" className="p-5 text-blue-400 font-bold">{fightClass.className}</a>
                                     <div className="gap-2 flex items-end">
@@ -46,12 +61,11 @@ const Classes = () => {
                                         <BsStarFill></BsStarFill>
                                         <BsStarHalf></BsStarHalf>
                                         <p>{fightClass.rating} <span className="text-violet-500"> ({fightClass.enrolled})</span></p>
-                                        <button onClick={handleOrderConfirm} className={`text-lg md:ml-5 font-bold p-1 rounded-xl  text-white bg-rose-500 ${fightClass.availableSeats===0 ? 'bg-gray-400 btn-disabled':''} `}>select</button>
+                                        <button onClick={()=>handleSelectClass(fightClass)} className={`text-lg md:ml-5 font-bold p-1 rounded-xl  text-white bg-rose-500 ${fightClass.availableSeats === 0 ? 'bg-gray-400 btn-disabled' : ''} `}>select</button>
                                     </div>
                                 </div>
                             </div>
                         </>
-
                     })
                 }
             </div>
--- src/components/Pages/Dashboard/StudentDashboard.jsx
+++ src/components/Pages/Dashboard/StudentDashboard.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const StudentDashboard = ({student}) => {
+const StudentDashboard = ({ student }) => {
     return (
         <div>
             <h1 className='text-4xl'>{student.name}</h1>
+            <ul className="bg-rose-500 w-52 h-[100vh]">
+                <li className='font-bold text-2xl text-white px-3 py-4'>Selected Class</li>
+                <li className='font-bold text-2xl text-white px-3 py-4'>Enrolled Class</li>
+            </ul>
         </div>
     );
 };
